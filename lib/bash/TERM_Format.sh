2f2470941a8ed8b27433d2c4575ab25f214b5e2a
Weibin <weibin.zhang@stonybrook.edu>
2019-09-20 10:07:02 -0400

    Weekly backup

--- lib/bash/TERM_Format.sh
+++ lib/bash/TERM_Format.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+############################################
+# 1 for bold
+# 2 for dim
+# 4 for underline
+# 5 for blink 
+# 7 for inverse
+# 39 for default foreground color
+# 30-37 for foreground colors
+# 90-97 for 16-color mode foreground colors
+# 38;5;0 to 38;5;255 for 88-color and 256-color modes foreground colors
+# 49 for default background color
+# 40-47 for background colors
+# 100-107 for 16-color mode background colors
+# 48;5;0 to 48;5;255 for 88-color and 256-color modes background colors
+############################################
 bold()
 {
     echo -ne "\e[1m$*\e[21m"
